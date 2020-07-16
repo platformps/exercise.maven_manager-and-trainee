4b1e9fc0cf93657ebd48d8693a417946459663e7
CJ Fulton <fulchr3356@gmail.com>
2020-07-16 15:46:48 -0400

    completed exercise

--- src/main/java/com/github/perscholas/AbstractEmployee.java
+++ src/main/java/com/github/perscholas/AbstractEmployee.java
@@ -5,8 +5,17 @@ package com.github.perscholas;
  */
 // TODO - Ensure an `AbstractEmployee` implements the `EmployeeInterface`
 // TODO - Ensure an `AbstractEmployee` is abstract
-public class AbstractEmployee {
+public class AbstractEmployee implements EmployeeInterface {
     private Object exampleOfInstanceVariable; // TODO - Replace with correct instance variable declarations
+    private Long id;
+    private String name;
+    private String address;
+    private Long phoneNumber;
+    private Double basicSalary;
+    private Double specialAllowance = 250.8;
+    private Double healthReimbursementAccount = 1000.5;
+
+
 
     // TODO - Ensure that each of the arguments passed through the constructor are assigned to a respective instance variable
     private AbstractEmployee(Long id, String name, String address, Long phoneNumber, Double basicSalary, Double specialAllowance, Double healthReimbursementAccount) {
@@ -17,5 +26,80 @@ public class AbstractEmployee {
     }
 
     public AbstractEmployee() {
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public Long getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    @Override
+    public Double getBasicSalary() {
+        return this.basicSalary;
+    }
+
+    @Override
+    public Double getSpecialAllowance() {
+        return this.specialAllowance;
+    }
+
+    @Override
+    public Double getHealthReimbursementAccount() {
+
+        return this.healthReimbursementAccount;
+    }
+
+     public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void setBasicSalary(Double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    @Override
+    public void setSpecialAllowance(Double specialAllowance) {
+        this.specialAllowance =specialAllowance;
+    }
+
+    @Override
+    public void setHealthReimbursementAccount(Double healthReimbursementAccount) {
+        this.healthReimbursementAccount = healthReimbursementAccount;
+    }
+
+    @Override
+    public Double calculateSalary() {
+        return null;
     }
 }
--- src/main/java/com/github/perscholas/Manager.java
+++ src/main/java/com/github/perscholas/Manager.java
@@ -3,12 +3,30 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class Manager {
+public class Manager extends AbstractEmployee {
+
+    private  Long id;
+    private  String name;
+    private  String address;
+    private  Long phoneNumber;
+    private  Double baseSalary;
+
+
     // TODO - Ensure that each of the arguments passed through the constructor are assigned to a respective instance variable
     public Manager(Long id, String name, String address, Long phoneNumber, Double basicSalary) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setPhoneNumber(phoneNumber);
+        this.setBasicSalary(basicSalary);
     }
+
 
     // TODO - Modify to ensure the respective test case passes
     public Manager() {
+        this.setSpecialAllowance(250.80);
+        this.setHealthReimbursementAccount(1000.5);
+
+
     }
 }
