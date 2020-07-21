ddbc9c5d9b2fee3f0a68933192c17401bd093bc6
unknown <dyirchott@hotmail.com>
2020-07-21 11:53:47 -0400

    completed exercise

--- src/main/java/com/github/perscholas/Trainee.java
+++ src/main/java/com/github/perscholas/Trainee.java
@@ -3,12 +3,16 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class Trainee {
-    // TODO - Ensure that each of the arguments passed through the constructor are assigned to a respective instance variable
+public class Trainee extends AbstractEmployee{
     public Trainee(Long id, String name, String address, Long phoneNumber, Double basicSalary) {
+    super(id, name, address, phoneNumber, basicSalary);
     }
 
-    // TODO - Modify to ensure the respective test case passes
     public Trainee() {
+    }
+
+    @Override
+    public void calculateSalary() {
+
     }
 }
