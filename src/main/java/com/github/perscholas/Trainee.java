b1c37574c1fddb30fbd5c2cb77a46c3bb97af579
dmholland <kenoshatrader@gmail.com>
2020-07-21 12:56:04 -0400

    Done -David

--- src/main/java/com/github/perscholas/Trainee.java
+++ src/main/java/com/github/perscholas/Trainee.java
@@ -3,12 +3,21 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class Trainee {
+public class Trainee  extends AbstractEmployee{
     // TODO - Ensure that each of the arguments passed through the constructor are assigned to a respective instance variable
+
     public Trainee(Long id, String name, String address, Long phoneNumber, Double basicSalary) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setPhoneNumber(phoneNumber);
+        this.setBasicSalary(basicSalary);
     }
+
 
     // TODO - Modify to ensure the respective test case passes
     public Trainee() {
     }
+
+
 }
