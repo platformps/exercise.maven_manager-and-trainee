7b2db618264d8fc44db082be0ca083d6a814ab85
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-07-21 22:07:06 -0400

    mimicked what Leon modeled for us in class since I had a hard time following the directives in the README. Will revisit for adding in the transport allowance calculation for employees.

--- src/main/java/com/github/perscholas/Manager.java
+++ src/main/java/com/github/perscholas/Manager.java
@@ -3,12 +3,18 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class Manager {
-    // TODO - Ensure that each of the arguments passed through the constructor are assigned to a respective instance variable
+public class Manager extends AbstractEmployee {
+
     public Manager(Long id, String name, String address, Long phoneNumber, Double basicSalary) {
+        super(id, name, address, phoneNumber, basicSalary, 250.80, 1000.50);
     }
 
     // TODO - Modify to ensure the respective test case passes
     public Manager() {
+        super(null, null, null, null, null, 250.80, 1000.50);
+    }
+
+    public void calculateSalary() {
+
     }
 }
